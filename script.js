67dcf667519413c505a332677d49418ed2710ff6
ievale23 <ieva.lebedyte@gmail.com>
2023-02-20 14:39:21 +0200

    veikia!

--- script.js
+++ script.js
@@ -9,4 +9,35 @@ Pastaba: Informacija apie automobilį (brand) (jo kortelė) turi turėti
 bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
-//dirbam ant dev hello
+async function carList() {
+  try {
+    let response = await fetch("./cars.json", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.ok) {
+      let result = await response.json();
+      console.log(result);
+
+      result.forEach((element) => {
+        let carCard = document.createElement("div");
+        carCard.classList.add("carCard");
+
+        let brandBox = document.createElement("h2");
+        brandBox.textContent = element.brand;
+
+        let modelBox = document.createElement("p");
+        modelBox.textContent = element.models;
+
+        carCard.append(brandBox, modelBox);
+        document.querySelector("#output").append(carCard);
+      });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+carList();
